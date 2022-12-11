51c915cd118498c32ea8ecf8a5dac313c826c9d2
gypalo911 <gypalo911@gmail.com>
2022-12-11 16:01:09 +0200

    Add simple authentication using auth0

---
+++ android/app/src/main/kotlin/com/example/ano_geo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ano_geo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_appauth
+import flutter_secure_storage_macos
+import path_provider_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
+  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
